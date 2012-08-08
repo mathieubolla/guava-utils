3f561bdea3acbabfdd7a60be0db8374ddda4dfbb
Mathieu Bolla <mbolla@splayce.com>
2012-08-08 14:29:31 +0200

    Prove parallelism

--- src/test/java/FluentParallelIterableTest.java
+++ src/test/java/FluentParallelIterableTest.java
@@ -5,6 +5,8 @@ import com.mathieubolla.guava.FluentParallelIterable;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -33,5 +35,51 @@ public class FluentParallelIterableTest {
 		}, 2);
 
 		assertThat(parallelPositives).containsOnly(1, 2);
+	}
+
+	@Test(timeout = 200)
+	public void shouldTransformInParallel() {
+		final AtomicBoolean hasBeenInterrupted = new AtomicBoolean(false);
+		final CountDownLatch lock = new CountDownLatch(2);
+		FluentParallelIterable<Integer> fluentParallelIterable = FluentParallelIterable.from(Arrays.asList(1, 2));
+
+		FluentIterable<Integer> interlockingTransform = fluentParallelIterable.parallelTransform(new Function<Integer, Integer>() {
+			public Integer apply(Integer input) {
+				lock.countDown();
+				try {
+					lock.await();
+				} catch (InterruptedException e) {
+					hasBeenInterrupted.set(true);
+					throw new RuntimeException(e);
+				}
+				return input;
+			}
+		}, 2);
+
+		assertThat(hasBeenInterrupted.get()).isFalse();
+		assertThat(interlockingTransform).containsOnly(1, 2);
+	}
+
+	@Test(timeout = 200)
+	public void shouldFilterInParallel() {
+		final AtomicBoolean hasBeenInterrupted = new AtomicBoolean(false);
+		final CountDownLatch lock = new CountDownLatch(2);
+		FluentParallelIterable<Integer> fluentParallelIterable = FluentParallelIterable.from(Arrays.asList(1, 2));
+
+		FluentIterable<Integer> interlockingTransform = fluentParallelIterable.parallelFilter(new Predicate<Integer>() {
+			public boolean apply(Integer input) {
+				lock.countDown();
+				try {
+					lock.await();
+				} catch (InterruptedException e) {
+					hasBeenInterrupted.set(true);
+					throw new RuntimeException(e);
+				}
+				return true;
+			}
+		}, 2);
+
+		assertThat(hasBeenInterrupted.get()).isFalse();
+		assertThat(interlockingTransform).containsOnly(1, 2);
 	}
 }
